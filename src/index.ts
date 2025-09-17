6b41c511cd80804ad2f38a46f4eeec7ba701d82c
Viettrung <nguyenviettrung2005@gmail.com>
2025-09-17 13:17:31 +0700

    set up

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import express from 'express';
 
 dotenv.config();
 
@@ -10,13 +10,14 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    const a =       1
-    const b =    2
+    const a = 1;
+    const b = 2;
 
-    
     res.send('Hello');
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+    console.log(
+        `Server is running on port http://localhost:${process.env.PORT}`
+    );
 });
